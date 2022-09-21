78e4feaa57cb2037c560b34caaa301efa1bffb03
muaxz <bexsd@hotmail.com>
2022-09-20 23:32:50 -0400

    change

--- client/src/components/navigation/navigation.tsx
+++ client/src/components/navigation/navigation.tsx
@@ -4,7 +4,7 @@ import {faCartShopping,faXmark} from "@fortawesome/free-solid-svg-icons"
 import { Link } from 'react-router-dom';
 import {useDispatch,useSelector} from "react-redux"
 import {incrementListCount,productSlice,populateList} from "../../state/slices/product"
-import {useQuery} from "@apollo/client"
+import {useLazyQuery,useQuery} from "@apollo/client"
 import {getCartCount,searchProducts} from "../../GraphQL/Queries"
 import {RootState} from "../../state/store"
 
@@ -14,7 +14,8 @@ function NavigationBar(){
   const cartCount = useSelector((state: RootState)=>state.product.listCount)
   const dispatch = useDispatch()
   const [searchValue,setSearchValue] = useState<string>("")
-  const {data:searchData,loading:searchLoading,error:searchError,refetch} = useQuery(searchProducts,{variables:{searchValue:searchValue}})
+  const [categoryList,setCategoryList] = useState([])
+  const [refetch,{data:searchData,loading:searchLoading,error:searchError}] = useLazyQuery(searchProducts)
   const {data,loading,error} = useQuery(getCartCount,{variables:{userId:localStorage.getItem("sessionId")}})
 
   useEffect(()=>{
@@ -23,15 +24,26 @@ function NavigationBar(){
 
   },[data])
 
+  useEffect(()=>{
+    if(searchData)
+    setCategoryList(searchData.searchProduct)
+  },[searchData])
+
   const searchHandler = (e : React.FormEvent<HTMLInputElement>)=>{
      setSearchValue(e.currentTarget.value)
-     if(searchValue.trim() !== "")
-     refetch({searchValue:searchValue})
+     if(e.currentTarget.value === ""){
+        setCategoryList([])
+     }
   }
 
   const linkAction = ()=>{
     dispatch(populateList([]))
     setSearchValue("")
+  }
+
+  const submitSearch=()=>{
+    if(searchValue.trim() !== "")
+    refetch({variables:{searchValue:searchValue}})
   }
 
   return (
@@ -41,17 +53,20 @@ function NavigationBar(){
            <h1 className='text-2xl'>E-Commerce</h1>
         </Link>
         <div className="w-64 h-10">
-          <input value={searchValue} onChange={searchHandler} placeholder="Search Product..." className="pl-2 h-full border-solid border-2 border-slate-200 w-full rounded-lg" type="text" />
+          <input onKeyUp={submitSearch} value={searchValue} onChange={searchHandler} placeholder="Search Product..." className="pl-2 h-full border-solid border-2 border-slate-200 w-full rounded-lg" type="text" />
         </div>
         <div className={`w-64 h-64 ${searchValue.length  ?  "block" : "hidden"} overflow-auto pr-2 pl-2 bg-slate-200 rounded absolute top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
             <div onClick={()=>setSearchValue("")} className="text-right p-2 cursor-pointer"><FontAwesomeIcon icon={faXmark}></FontAwesomeIcon></div>
-            {searchLoading ? "" : (
-              searchData.searchProduct.map((item:{name:string,id:number},index:number)=>(
+            {searchLoading ? <div style={{display:"flex",justifyContent:"center",position:"relative",top:"30px",right:"21px"}}><div className="lds-ring"><div></div><div></div><div></div><div></div></div></div> : (
+              categoryList.map((item:{name:string,id:number},index:number)=>(
                 <Link onClick={linkAction} to={`/?category=${item.id}`}>
                    <div className="hover:bg-red-200 hover:text-white p-2 cursor-pointer rounded" key={index}>{item.name}</div>
                 </Link>
               ))
             )}
+            {
+              !categoryList.length && <div className='pl-5'>There is no such a product...</div>
+            }
         </div>
         <Link to={"/cart"}>
             <button className='relative'>
