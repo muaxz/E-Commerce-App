8531d0a26187c770eaf33aeafb9bafbef22aed69
muaxz <bexsd@hotmail.com>
2022-10-03 13:10:05 -0400

    change

--- backend/DataBase/connection.js
+++ backend/DataBase/connection.js
@@ -1,7 +1,12 @@
 const {Sequelize} = require("sequelize")
 
 
-const sequelizeBase = new Sequelize("heroku_b90041b976bbb35","bee24fb0efc68b","8468f771",{host:"us-cdbr-east-06.cleardb.net",dialect:"mysql"})
+const sequelizeBase = new Sequelize("heroku_b90041b976bbb35","bee24fb0efc68b","8468f771",{host:"us-cdbr-east-06.cleardb.net",dialect:"mysql",pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 60000
+  }})
 
 sequelizeBase.authenticate().then(()=>console.log("connected!"))
 
